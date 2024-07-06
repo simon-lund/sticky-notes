aad952bc6bd9a7adb7ad8564b70fcfd3e8a8071e
Simon Lund <simon97lund@gmail.com>
2024-07-06 14:50:24 +0200

    :heavy_plus_sign: add svelte-sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.css';
 	import Pinboard from '$lib/components/pinboard.svelte';
+	import { Toaster } from 'svelte-sonner';
 </script>
 
+<Toaster />
 <Pinboard>
 	<slot />
 </Pinboard>
